8a4cef5486acc3d961565bf4aca8e2697eb85212
grandomx <grandomx@gmail.com>
2021-07-26 00:18:58 +0700

    update user session

--- js/script.js
+++ js/script.js
@@ -1,25 +1,3 @@
-window.onload = function() {
-    var userSession = localStorage.getItem("sessionStorage");
-    var sessionCid = JSON.parse(userSession);
-    var rtData = localStorage.getItem("usersList");
-    var storedUsers = JSON.parse(rtData);
-    var storedName;
-
-    for (var a = 0; a < storedUsers.length; a++) {
-        var obj = storedUsers[a];
-        
-        storedName = obj.name;
-    }
-
-    if (sessionCid == "cid") {
-        var nama = storedName;
-        var uname = document.getElementById("username");
-        var btn = document.getElementById("sign-btn");
-        uname.innerHTML = nama;
-        btn.innerHTML = "<i class='fa fa-sign-out'></i> Logout";
-    }
-}
-
 function togglePanel() {
     var sp = document.getElementById("side-toggle");
     
